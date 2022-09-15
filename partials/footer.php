a57a8aa6a997377801f61918faca74b52910c179
MUHAMMED YASEEN K C <ymkcmpd@gmail.com>
2022-09-15 14:45:33 +0530

    dfgsd

---
+++ partials/footer.php
@@ -0,0 +1,91 @@
+<!-- Footer Start -->
+<div class="container-fluid bg-dark footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
+    <div class="container py-5">
+        <div class="row g-5">
+            <div class="col-lg-4 col-md-6">
+                <h2 class="text-white mb-4">
+                    <img class="img-fluid me-3" src="../assets/img/cusat-tech-logo.png" alt="" />CUSAT TBI
+                </h2>
+                <p>
+                    Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat
+                    ipsum et lorem et sit, sed stet lorem sit clita
+                </p>
+                <div class="d-flex pt-2">
+                    <a class="btn btn-square me-1" href="">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                    <a class="btn btn-square me-1" href="">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+                    <a class="btn btn-square me-1" href="">
+                        <i class="fab fa-youtube"></i>
+                    </a>
+                    <a class="btn btn-square me-0" href="">
+                        <i class="fab fa-linkedin-in"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <h5 class="text-light mb-4">Address</h5>
+                <p>
+                    <i class="fa fa-map-marker-alt me-3"></i>Room No. 310,</br>
+                    Administrative Building</br>
+                    Cochin University of Science and Technology (CUSAT)</br>
+                    KOCHI - 682022, Ernakulam,</br>
+                    Kerala, India.
+                </p>
+                <p>
+                    <i class="fa fa-phone-alt me-3"></i>+91-484-2575396
+                </p>
+                <p>
+                    <i class="fa fa-phone-alt me-3"></i>+91-7907062396
+                </p>
+                <p>
+                    <i class="fa fa-envelope me-3"></i>cusatechfoundation@cusat.ac.in
+                </p>
+                <p>
+                    <i class="fa fa-envelope me-3"></i>cusatechoundation@gmail.com
+                </p>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <h5 class="text-light mb-4">Browse Pages</h5>
+                <a class="btn btn-link" href="">About Us</a>
+                <a class="btn btn-link" href="">RISE TBI</a>
+                <a class="btn btn-link" href="">CITTIC</a>
+                <a class="btn btn-link" href="">CUSAT Tech Foundation</a>
+                <a class="btn btn-link" href="">FAB LAB</a>
+                <a class="btn btn-link" href="">StartUp</a>
+                <a class="btn btn-link" href="">Funding</a>
+                <a class="btn btn-link" href="">Eventss</a>
+
+
+            </div>
+            <!-- <div class="col-lg-3 col-md-6">
+                <h5 class="text-light mb-4">Newsletter</h5>
+                <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
+                <div class="position-relative mx-auto" style="max-width: 400px">
+                    <input class="form-control bg-transparent w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email" />
+                    <button type="button" class="btn btn-secondary py-2 position-absolute top-0 end-0 mt-2 me-2">
+                        SignUp
+                    </button>
+                </div>
+            </div> -->
+        </div>
+    </div>
+    <div class="container-fluid copyright">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                    &copy; <a href="https://infinio.co.in/">Infinio.co.in</a> All Right Reserved.
+                </div>
+                <div class="col-md-6 text-center text-md-end">
+                    <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
+                    Designed By <a href="https://infinio.co.in/">Infinio Technology</a>
+                    <!-- <br />Distributed By: -->
+                    <!-- <a href="https://themewagon.com" target="_blank">ThemeWagon</a> -->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Footer End -->
---
+++ partials/header.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg bg-whiteNew navbar-light sticky-top px-4 px-lg-5">
+    <a href="../index.html" class="navbar-brand d-flex align-items-center">
+        <h1 class="m-0">
+            <img class="img-fluid me-3" src="../assets/img/cusat-tech-logo.png" alt="" />CUSAT TBI
+        </h1>
+    </a>
+    <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav mx-auto bg-light rounded pe-4 py-3 py-lg-0">
+            <!-- <a href="../team/team.php" class="nav-item nav-link">About Us</a>
+            <a href="../team/rise.php" class="nav-item nav-link">RISE TBI</a>
+            <a href="../team/rise.php" class="nav-item nav-link">CITTIC</a>
+            <a href="../team/cusattechfoundation.php" class="nav-item nav-link">CUSAT Tech Foundation</a>
+            <a href="../team/fablab.php" class="nav-item nav-link">FABLAB</a>
+            <a href="#" class="nav-item nav-link">Startup</a>
+            <a href="#" class="nav-item nav-link">Funding</a>
+            <a href="#" class="nav-item nav-link">Events</a> -->
+        </div>
+    </div>
+</nav>
